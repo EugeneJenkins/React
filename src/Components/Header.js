29fb1e1ae7c441e4b9e4e04a1da29c9a5d736fa3
EugeneJenkins <skreker99@gmail.com>
2021-06-27 18:01:29 +0300

    added new components

---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div>
+            <a href='#'>Test1</a>
+            &nbsp;
+            <a href='#' >Test2</a>
+        </div>
+    );
+}
+
+export default Header;
---
+++ src/Components/Techno.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Techno = () => {
+    return (
+        <div>
+            <ul>
+                <li> first text</li>
+                <li> second text</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Techno;
