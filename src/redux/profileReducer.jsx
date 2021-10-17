fb8421e2ae8d70e16ac97ae8b2c9f0d3bd6e8aed
EugeneJenkins <skreker99@gmail.com>
2021-10-17 14:33:06 +0300

    lesson 41

--- src/redux/profileReducer.jsx
+++ src/redux/profileReducer.jsx
@@ -1,7 +1,16 @@
 const ADD_POST = 'ADD-POST';
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 
-const profileReducer = (state, action) => {
+const initialState = {
+  posts: [
+    { id: 1, message: 'Hi', likesCount: 1 },
+    { id: 2, message: 'How are you', likesCount: 12 },
+    { id: 3, message: 'i am fine, and you', likesCount: 20 },
+  ],
+  newPostText: 'Test this test',
+};
+
+const profileReducer = (state = initialState, action) => {
   const addPost = () => {
     const newPost = { id: 5, message: state.newPostText, likesCount: 12 };
 
